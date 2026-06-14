7078a9d5dc31b41ffa4e63eaa1b41807bf22c395
Andezion <245122@edu.p.lodz.pl>
2026-06-14 17:09:30 +0200

    add: added new stuff to the admin

--- src/main/java/com/andezion/firstplugin/FirstPlugin.java
+++ src/main/java/com/andezion/firstplugin/FirstPlugin.java
@@ -1,22 +1,28 @@
 package com.andezion.firstplugin;
 
+import com.andezion.firstplugin.commands.ProtectCommand;
+import com.andezion.firstplugin.listeners.BlockProtectionListener;
 import com.andezion.firstplugin.listeners.PlayerEventListener;
+import com.andezion.firstplugin.managers.ProtectionManager;
 import com.andezion.firstplugin.managers.RankManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirstPlugin extends JavaPlugin {
 
     private RankManager rankManager;
+    private ProtectionManager protectionManager;
 
     @Override
     public void onEnable() {
         getLogger().info("FirstPlugin is now enabled!");
 
         rankManager = new RankManager();
+        protectionManager = new ProtectionManager(this);
 
-        getServer().getPluginManager().registerEvents(
-                new PlayerEventListener(rankManager), this
-        );
+        getServer().getPluginManager().registerEvents(new PlayerEventListener(rankManager), this);
+        getServer().getPluginManager().registerEvents(new BlockProtectionListener(protectionManager), this);
+
+        getCommand("protect").setExecutor(new ProtectCommand(protectionManager));
     }
 
     @Override
